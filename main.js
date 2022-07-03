11acfeef498432453d4dcca414df86e1a5fd2149
Santiago Inostroza <santiagoinostroza2@gmail.com>
2022-07-03 14:24:57 -0400

    primer commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <p>Desplegando proyecto de prueba con Vite</p>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
